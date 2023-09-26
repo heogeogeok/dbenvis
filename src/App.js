c3d826c1c4adc054df4c10a227c7f33af03b69fd
juheehan@dbs.snu.ac.kr <juheehan@dbs.snu.ac.kr>
2023-09-26 21:31:49 +0900

    removed everything in App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Drawing from "./components/Drawing";
 function App() {
   return (
     <div className="App">
       {"Hello World!!"}
+      <Drawing/>
 </div> );
 }
 export default App;
---
+++ src/components/Drawing.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Drawing = (props) => {
+  return (
+    <div>
+      {"Place to draw something"}
+</div> )
+}
+export default Drawing;
