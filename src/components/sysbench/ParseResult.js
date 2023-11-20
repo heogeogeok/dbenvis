cb0fd8c30598646bf19e391c23abe693ec5ac642
danny-kim2611 <creative2613@gmail.com>
2023-11-20 22:04:16 +0900

    Separate Line chart

--- src/components/sysbench/ParseResult.js
+++ src/components/sysbench/ParseResult.js
@@ -1,14 +1,7 @@
-import { useRef, useEffect, useState } from "react";
-import * as d3 from "d3";
+import { useEffect, useState } from "react";
+import LineChart from "./LineChart";
 
 const ParseResult = ({ files }) => {
-  const lineplotSvg = useRef(null);
-
-  const width = 500;
-  const height = 300;
-  const marginX = 50;
-  const marginY = 20;
-
   const [contents, setContents] = useState([]);
   const [queryResults, setQueryResults] = useState([]);
 
@@ -57,72 +50,18 @@ const ParseResult = ({ files }) => {
         });
       }
     }
-
     setQueryResults(results);
   }, [contents]);
-
-  /* TODO: line chart 그리는 LineChart.js 만들어서 분리하기 */
-  useEffect(() => {
-    drawLineChart({
-      chartSvg: lineplotSvg,
-      data: queryResults,
-    });
-  }, [queryResults]);
-
-  function drawLineChart(props) {
-    const { chartSvg, data } = props;
-    const svg = d3.select(chartSvg.current);
-
-    svg.selectAll("*").remove(); // clear
-
-    // create scales for x and y
-    const xScale = d3
-      .scaleLinear()
-      .domain([d3.min(data, (d) => d.time), d3.max(data, (d) => d.time)])
-      .range([marginX, width - marginY]);
-
-    const yScale = d3
-      .scaleLinear()
-      .domain([d3.min(data, (d) => d.tps), d3.max(data, (d) => d.tps)])
-      .range([height - marginY, marginX]);
-
-    // create x and y axes
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-
-    // draw x and y axes
-    svg
-      .append("g")
-      .attr("transform", `translate(${0}, ${height - marginY})`)
-      .call(xAxis);
-
-    svg.append("g").attr("transform", `translate(${marginX}, 0)`).call(yAxis);
-
-    // draw line
-    const line = d3
-      .line()
-      .x((d) => xScale(d.time))
-      .y((d) => yScale(d.tps));
-
-    svg
-      .append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "#4ab180")
-      .attr("stroke-width", 1)
-      .attr("d", line);
-  }
 
   return (
     <div>
       <h1 className="title">Benchmark Result</h1>
-      {contents.length > 0 && (
-        <svg
-          ref={lineplotSvg}
-          width={width + 2 * marginX}
-          height={height + 2 * marginY}
-        ></svg>
-      )}
+      <div className="Line-container">
+        <LineChart
+          // key={index}
+          queryResults={queryResults}
+        />
+      </div>
     </div>
   );
 };
