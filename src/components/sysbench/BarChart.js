defed5c7508ceea136bdf46009b824ee20e1c8bc
yoojin-dbs <cyj@dbs.snu.ac.kr>
2023-12-08 19:34:02 +0900

    sysbench 수정

--- src/components/sysbench/BarChart.js
+++ src/components/sysbench/BarChart.js
@@ -1,139 +1,102 @@
 import * as d3 from "d3";
 import { useRef, useEffect, useState } from "react";
 
-const BarChart = ({ files, ...props }) => {
+const BarChart = (props) => {
+  const { files, avgTps } = props;
   const barplotSvg = useRef(null);
 
   const width = document.body.clientWidth * 0.3;
   const height = 200;
-  const marginX = document.body.clientWidth * 0.05;
+  const marginX = document.body.clientWidth * 0.25;
   const marginY = 30;
-  const barPadding = 0.3;
-
-  const [queryResults, setQueryResults] = useState([]);
-
-  const dbname = ["PostgreSQL", "MariaDB", "SQL Server", "MySQL"];
 
   useEffect(() => {
-    const loadFiles = async () => {
-      if (files && files.length > 0) {
-        const fileContents = [];
-
-        let i = 0;
-
-        for (const file of files) {
-          const fileContent = await readFile(file);
-          const results = extractAvgTps(fileContent);
-
-          fileContents.push({ name: dbname[i], transactionsPerSec: results });
-          i++;
-        }
-
-        setQueryResults(fileContents);
-      } else {
-        // 업로드 한 파일 없는 경우
-        setQueryResults([]);
-      }
-    };
-
-    loadFiles();
-  }, [files]);
-
-  const readFile = (file) => {
-    return new Promise((resolve) => {
-      const fileReader = new FileReader();
-
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-
-      // read the file as text
-      fileReader.readAsText(file);
-    });
-  };
-
-  /* input preprocessinge */
-  const extractAvgTps = (content) => {
-    const regex = /transactions:\s+\d+\s+\(([\d.]+)\s+per sec.\)/;
-    const match = content.match(regex);
-
-    if (match) {
-      const transactionsPerSec = parseFloat(match[1]); // per sec 값 추출
-      return transactionsPerSec;
-    } else {
-      console.error("Unable to extract average from the content");
-      return null;
-    }
-  };
-
-  useEffect(() => {
-    drawBarChart({
-      chartSvg: barplotSvg,
-      data: queryResults,
-    });
-  }, [queryResults]);
-
-  function drawBarChart(props) {
-    const { chartSvg, data } = props;
-    const svg = d3.select(chartSvg.current);
-
+    const svg = d3.select(barplotSvg.current);
     svg.selectAll("*").remove(); // clear
 
     // create scales for x and y
     const xScale = d3
       .scaleBand()
-      .domain(data.map((entry) => entry.name))
+      .domain(avgTps.map((entry, index) => index))
       .range([0, width])
       .align(0.5)
-      .padding(barPadding);
+      .padding(0.1);
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.transactionsPerSec)])
+      .domain([0, d3.max(avgTps)])
       .range([height, 0]);
 
+    // create color scale for each bars in the group
     const colorScale = d3
       .scaleOrdinal()
-      .domain(data.map((entry) => entry.name))
-      .range(d3.schemeCategory10);
+      .domain(avgTps.map((entry, index) => index))
+      .range([
+        "#7fc97f",
+        "#beaed4",
+        "#fdc086",
+        "#ffff99",
+        "#386cb0",
+        "#f0027f",
+        "#bf5b17",
+        "#666666",
+      ]);
 
     // create x and y axes
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale).tickFormat((index) => {
+      // 파일 이름이 긴 경우 truncate
+      if (files[index]) {
+        const tick =
+          files[index].name.length > 45 / files.length
+            ? `${files[index].name.slice(0, 45 / files.length)}...`
+            : files[index].name;
+        return tick;
+      }
+    });
     const yAxis = d3.axisLeft(yScale);
 
     // draw x and y axes
     svg
       .append("g")
+      .attr("class", "x-axis")
       .attr("transform", `translate(${marginX}, ${height + marginY})`)
+      .transition()
+      .duration(1000)
       .call(xAxis);
 
     svg
       .append("g")
+      .attr("class", "y-axis")
       .attr("transform", `translate(${marginX}, ${marginY})`)
+      .transition()
+      .duration(1000)
       .call(yAxis);
 
     // draw bars
     svg
       .append("g")
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", (d) => xScale(d.name) + marginX)
-      .attr("y", (d) => yScale(d.transactionsPerSec) + marginY)
+      .selectAll()
+      .data(avgTps)
+      .join("rect")
+      .attr("x", (d, index) => xScale(index) + marginX)
+      .attr("y", height + marginY) // transition: 초기 y position 맨 아래에
       .attr("width", xScale.bandwidth())
-      .attr("height", (d) => height - yScale(d.transactionsPerSec))
-      .attr("fill", (d) => colorScale(d.name));
-  }
+      .attr("height", 0) // transition: 초기 height 0
+      .attr("fill", (d, index) => colorScale(index))
+      .transition()
+      .duration(1000)
+      .attr("y", (d) => yScale(d) + marginY) // transition: final y position
+      .attr("height", (d) => height - yScale(d)); // transition: final height
+  }, [files, avgTps]);
+
   return (
-    <>
-      <h1 className="title">Average TPS</h1>
+    <div>
       <svg
         ref={barplotSvg}
         width={width + 2 * marginX}
         height={height + 2 * marginY}
       ></svg>
-    </>
+    </div>
   );
 };
 
--- src/components/sysbench/Sysbench.js
+++ src/components/sysbench/Sysbench.js
@@ -1,14 +1,120 @@
-import ParseResult from "./ParseResult";
+import { useEffect, useState } from "react";
+import LineChart from "./LineChart";
+import BarChart from "./BarChart";
 import "../../assets/stylesheets/Sysbench.css";
 
-function Sysbench(props) {
+const Sysbench = (props) => {
+  const files = props.files;
+
+  const [queryResults, setQueryResults] = useState([]);
+  const [avgTps, setAvgTps] = useState([]);
+
+  useEffect(() => {
+    const loadFiles = async () => {
+      if (files && files.length > 0) {
+        const fileContents = [];
+        const avgTpsValues = [];
+
+        for (const file of files) {
+          const fileContent = await readFile(file);
+          const results = extractResults(fileContent);
+
+          fileContents.push({ results });
+          avgTpsValues.push(extractAvgTps(fileContent));
+        }
+
+        setQueryResults(fileContents);
+        setAvgTps(avgTpsValues);
+      } else {
+        // 업로드 한 파일 없는 경우
+        setQueryResults([]);
+        setAvgTps([]);
+      }
+    };
+
+    loadFiles();
+  }, [files]);
+
+  const readFile = (file) => {
+    return new Promise((resolve) => {
+      const fileReader = new FileReader();
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      // read the file as text
+      fileReader.readAsText(file);
+    });
+  };
+
+  /* input preprocessing + query result update */
+  const extractResults = (content) => {
+    const regex =
+      /\[\s*(\d+s)\s*\]\s*thds:\s*(\d+)\s*tps:\s*([\d.]+)\s*qps:\s*([\d.]+).*lat\s*\(ms,99%\):\s*([\d.]+)\s*err\/s:\s*([\d.]+)/;
+    let match = null;
+    const results = [];
+
+    const lines = content.toString().split("\n");
+
+    for (let line of lines) {
+      // Ssop at Latency histogram
+      if (line.includes("Latency histogram (values are in milliseconds)")) {
+        break;
+      }
+
+      match = line.match(regex);
+
+      if (match) {
+        const [_, time, thds, tps, qps, lat, err] = match;
+        results.push({
+          time: parseInt(time),
+          tps: parseFloat(tps),
+          qps: parseFloat(qps),
+          lat: parseFloat(lat),
+        });
+      }
+    }
+    return results;
+  };
+
+  const extractAvgTps = (content) => {
+    const regex = /transactions:\s+\d+\s+\(([\d.]+)\s+per sec.\)/;
+    const match = content.match(regex);
+
+    if (match) {
+      const transactionsPerSec = parseFloat(match[1]); // per sec 값 추출
+      return transactionsPerSec;
+    } else {
+      console.error("Unable to extract average from the content");
+      return null;
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <ParseResult files={props.files} />
+    <div className="sysbench-container">
+      <div className="chart-container">
+        <h1 className="title">sysbench Result</h1>
+        {queryResults.length > 0 && (
+          <div className="line-chart-container">
+            {queryResults.map((results, index) => (
+              <LineChart
+                key={index}
+                fileIndex={index}
+                files={files}
+                queryResults={results.results}
+                avgTps={avgTps[index]}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="chart-container">
+        <h1 className="title">Compare View</h1>
+        {queryResults.length > 0 && <BarChart files={files} avgTps={avgTps} />}
       </div>
     </div>
   );
-}
+};
 
 export default Sysbench;
