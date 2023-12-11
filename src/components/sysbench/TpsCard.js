bef7ef9f814370f17c636bb6a2381a96511216d0
yoojin-dbs <cyj@dbs.snu.ac.kr>
2023-12-11 09:56:59 +0900

    tps card header 수정

--- src/components/sysbench/TpsCard.js
+++ src/components/sysbench/TpsCard.js
@@ -3,15 +3,24 @@ import { Card } from "@material-tailwind/react";
 function TpsCard(props) {
   let data = 0;
 
-  if (props.metric === "tps") data = props.average.tps;
-  else if (props.metric === "qps") data = props.average.qps;
-  else if (props.metric === "lat") data = props.average.lat.percentile99;
+  let headerText = "";
+
+  if (props.metric === "tps") {
+    data = props.average.tps;
+    headerText = "Average TPS";
+  } else if (props.metric === "qps") {
+    data = props.average.qps;
+    headerText = "Average QPS";
+  } else if (props.metric === "lat") {
+    data = props.average.lat.percentile99;
+    headerText = "Average Latency";
+  }
 
   if (props.average) {
     return (
       <Card className="tps-card">
         <div className="tps-card-content">
-          <h1 className="tps-card-header">Average TPS</h1>
+          <h1 className="tps-card-header">{headerText}</h1>
           <p>{data.toFixed(2)}</p>
         </div>
       </Card>
